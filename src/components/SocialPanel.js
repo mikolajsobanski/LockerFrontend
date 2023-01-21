d75dc0ea161093913b7545e6fa540ebee06fd4a4
Mikolaj Sobanski <mikolajsobanski01@gmail.com>
2023-01-21 13:28:26 +0100

    fix icons in socialPanel

--- src/components/SocialPanel.js
+++ src/components/SocialPanel.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import {FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaLinkedin, FaEnvelope} from 'react-icons/fa'
+//import {FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaLinkedin, FaEnvelope} from 'react-icons/fa'
 
 function SocialPanel() {
   return (
         
         <div className='socialPanel-container'>
             <Row>
-              <Col md ={2}><a className='facebook' href='#'><FaFacebook />    Facebook</a></Col>
-              <Col md ={2}><a href='#'><FaYoutube />   Youtube</a></Col>
-              <Col md ={2}><a className='instagram' href='#'><FaInstagram />     Instagram</a></Col>
-              <Col md ={2}><a href='#'><FaEnvelope/>    Gmail</a></Col>
-              <Col md ={2}><a className='linkedin' href='#'><FaLinkedin/>    Linkedin</a></Col>
-              <Col md ={2}><a className='twitter' href='#'><FaTwitter/>      Twitter</a></Col>
+              <Col md ={2}><a className='facebook' href='#'>  Facebook</a></Col>
+              <Col md ={2}><a href='#'>  Youtube</a></Col>
+              <Col md ={2}><a className='instagram' href='#'>   Instagram</a></Col>
+              <Col md ={2}><a href='#'>   Gmail</a></Col>
+              <Col md ={2}><a className='linkedin' href='#'>    Linkedin</a></Col>
+              <Col md ={2}><a className='twitter' href='#'>      Twitter</a></Col>
             </Row>
         </div>
         
